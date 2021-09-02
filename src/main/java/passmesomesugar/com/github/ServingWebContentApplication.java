f5dbc93db76836c61ec4358bae7d7bc7302ac2f5
Sadvakassov Bagzhan <sadvakasov.bagjan@gmail.com>
2021-09-02 14:55:51 +0600

    genesis

---
+++ src/main/java/passmesomesugar/com/github/ServingWebContentApplication.java
@@ -0,0 +1,12 @@
+package passmesomesugar.com.github;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServingWebContentApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ServingWebContentApplication.class, args);
+  }
+}
